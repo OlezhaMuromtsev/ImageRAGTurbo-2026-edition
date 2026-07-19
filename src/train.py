4b8aea94de25df6d9c60a7fbc027cbf8911663b5
OlezhaMuromtsev <kukoldun1@yandex.ru>
2026-07-19 13:51:06 +0300

    added imageragturbo

--- src/train.py
+++ src/train.py
@@ -3,24 +3,24 @@ import argparse
 import numpy as np
 import torch
 from utils.config import Config
+from model.RAG.rag import RAG
 
 def main():
     parser = argparse.ArgumentParser(description=" ")
     parser.add_argument("--config", required=True, help="Path to train config")
     parser.add_argument("--path", type=str, default="model.pth", 
                         help="Path to save trained model")
-    parser.add_argument("--student_path", type=str, default="S.pth", 
-                        help="Path to save trained model")
-    parser.add_argument("--discriminator_path", type=str, default="D.pth", 
-                        help="Path to save trained model")
+
     args = parser.parse_args()
     config = Config(args.config)
     teacher = config.get_teacher()
     student = config.get_student()
     trainer = config.get_trainer()
-    trained_student_weights, trained_discriminator_weights = trainer.train(teacher, student)
-    torch.save(trained_student_weights, args.student_path)
-    torch.save(trained_discriminator_weights, args.discriminator_path)
+    trained = trainer.train(teacher, student)
+    checkpoint = {
+        'model_state_dict': trained.state_dict()
+    }
+    torch.save(checkpoint, args.path)
     return 0
 
 if __name__ == "__main__":
